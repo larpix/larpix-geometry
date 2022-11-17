067731402b2d4080a44bd700b69bb3380d01ba88
Stephen Greenberg <stephen_greenberg@berkeley.edu>
2022-11-17 13:06:40 +0100

    update agnostic hydra

--- larpixgeometry/layouts/make-agnostic-hydra.py
+++ larpixgeometry/layouts/make-agnostic-hydra.py
@@ -1,24 +1,33 @@
 import json
 io_groups = [1, 2]
-io_channels = [i for i in range(33)]
+tiles_per_io = [i for i in range(8)]
 chip_ids = [i for i in range(11, 112)]
 
+names = []
 
-net = {'network' : {}}
 for io_group in io_groups:
+	net = {'network' : {}}
 	net['network'][io_group] = {}
-	for io_channel in io_channels:
-		net['network'][io_group][io_channel] = {}
-		nodes = [ {"chip_id" : 'ext' }]
-		for chip in chip_ids:
-			nodes.append({'chip_id': chip})
-		net['network'][io_group][io_channel]['nodes']=nodes
+	for tile in tiles_per_io:
+		io_channels = [tile*4 + i + 1 for i in range(4)]
+		for io_channel in io_channels:
+			net['network'][io_group][io_channel] = {}
+			nodes = [ {"chip_id" : 'ext' }]
+			for chip in chip_ids:
+				nodes.append({'chip_id': chip})
+			net['network'][io_group][io_channel]['nodes']=nodes
 
 
-jsonString = json.dumps(net, indent=4)
-jsonFile = open('agnostic-hydra.json', "w")
-jsonFile.write(jsonString)
-jsonFile.close()
+		jsonString = json.dumps(net, indent=4)
+		names.append('io-{}-tile-{}-agnostic-hydra.json'.format(io_group, tile))
+		jsonFile = open('io-{}-tile-{}-agnostic-hydra.json'.format(io_group, tile), "w")
+		jsonFile.write(jsonString)
+		jsonFile.close()
+
+with open('agnostic-hydra.txt', 'w') as f:
+	for name in names:
+		f.write(name+'\n')
+
 
 
 
